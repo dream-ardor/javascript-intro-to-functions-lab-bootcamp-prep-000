a370bcb471c0a80355356ffd2f4411fb16f18594
Dan Barrick <barrick.dan80@gmail.com>
2018-08-25 19:40:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-function shout(string){
-  uppercase = string.toUpperCase();
-  string = uppercase;
-  return uppercase;
-}
-function whisper(string){
-  lowercase = string.toLowerCase();
-  string = lowercase;
-  return lowercase;
-}
-function logShout(string){
-  uppercase = string.toUpperCase()
-  string = uppercase;
-  console.log(uppercase);
-}
-function logWhisper(string){
-  lowercase = string.toLowerCase()
-  string = uppercase;
-  console.log(lowercase);
-}
+
 
 function sayHiToGrandma(string){
     if (string.toLowerCase() === string){

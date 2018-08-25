b015d884ca978da77405b8b6af4f9ab94147df6e
Dan Barrick <barrick.dan80@gmail.com>
2018-08-25 19:46:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ function shout(string) {
 }
 
 function logShout(string) {
-  console.log()
+  console.log(string.toUpperCase())
 }
 
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
 
 
 
